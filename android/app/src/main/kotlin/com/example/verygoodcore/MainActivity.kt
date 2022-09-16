cf135635caacebe6a737092af11d463c4910f49a
mercz <mercz@hotmail.com>
2022-09-16 17:06:46 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bloc05_todos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:bloc05_todos/app/app.dart';
+import 'package:bloc05_todos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
